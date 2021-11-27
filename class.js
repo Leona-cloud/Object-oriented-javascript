66c3b87cb5971dc5c512efb91b0728ebb9b03140
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2021-11-27 11:01:00 +0100

    Add files via upload

---
+++ class.js
@@ -0,0 +1,44 @@
+class User {
+    constructor(name, email, score){
+        this.email = email,
+        this.name = name,
+        this.score = 0
+    }
+
+    //methods
+    login(){
+        console.log(this.email, 'Just logged in');
+        return this;
+    }
+    logout(){
+        console.log(this.email, 'Just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+}
+
+class Admin extends User{
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email
+        })
+    }
+}
+
+
+const user1 = new User('Adanna', 'adanna@gmail.com');
+const user2 = new User('Funmilola', 'funmi1@gmail.com');
+const admin = new Admin('Chisom', 'chisom@gmail.com');
+
+//method chaining
+user1.login().updateScore().updateScore().logout();
+
+let users = [user1, user2, admin];
+
+admin.deleteUser(user2);
+
+console.log(users)
